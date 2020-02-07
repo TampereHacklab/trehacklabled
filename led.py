5405e7a1264b0f3d893ea6bf4a91959ec162f110
Tatu Wikman <tatu.wikman@gmail.com>
2020-02-07 22:19:28 +0200

    test autoupdate

--- led.py
+++ led.py
@@ -9,7 +9,7 @@ with serial.Serial("/dev/ttyS0", 4800, timeout=1) as ser:
         time_now = datetime.datetime.now()
         time_str = time_now.strftime("%H:%M:%S")
         timestuff = flipdot.Text(time_str, 0, 0, flipdot.Fonts.text_16px_bold)
-        hellotext = flipdot.Text("Dev me again!", 65, 0, flipdot.Fonts.text_16px_bold)
+        hellotext = flipdot.Text("Dev me more!", 65, 0, flipdot.Fonts.text_16px_bold)
         textstuff = flipdot.Text("TAMPERE HACKLAB", 0, 25, flipdot.Fonts.text_7px_bold)
         msg = flipdot.set_texts([timestuff, hellotext, textstuff])
         ser.write(msg)
